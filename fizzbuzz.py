dcd0fb04f68d83186af1a430b37ce7f69c027bad
pranavnatesh <p.n.ramanathan@student.tue.nl>
2023-05-17 18:47:59 +0200

    init

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+# write a fizzbuzz program that prints the numbers from 1 to 100
+
+for i in range(1, 11):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif i % 3 == 0:
+        print("fizz")
+        continue
+    elif i % 5 == 0:
+        print("buzz")
+        continue
+    print(i)
