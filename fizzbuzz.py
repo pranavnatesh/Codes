25405685298da15b901f94130aa039b1172db57c
pranavnatesh <p.n.ramanathan@student.tue.nl>
2023-05-17 18:49:38 +0200

    adjusted loop length from 11 to 101

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 # write a fizzbuzz program that prints the numbers from 1 to 100
 
-for i in range(1, 11):
+for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print("fizzbuzz")
         continue
